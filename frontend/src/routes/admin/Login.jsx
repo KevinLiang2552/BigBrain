0be36e16a633a79bb3aac2986967258312815122
nathan-lai234 <lai.nathan234@gmail.com>
2021-04-02 18:34:06 +1100

    Add base login page

--- frontend/src/routes/admin/Login.jsx
+++ frontend/src/routes/admin/Login.jsx
@@ -1,6 +1,40 @@
 import React from 'react';
-// import { Header } from '../../components/Header.jsx'
+import styles from '../../styles/auth.module.css';
+import { Link } from 'react-router-dom';
+import { Box, Button, Container, TextField } from '@material-ui/core';
 
 export const LoginPage = () => {
-  return <div>OO oo</div>;
+  return (
+    <>
+      <Container className={styles.mainBackground}>
+        <form className={styles.authForm}>
+          <h2>Login!</h2>
+          <Box mt={2}>
+            <TextField
+              id="loginEmail"
+              className={styles.authInput}
+              label="Email"
+              variant="outlined"
+            />
+          </Box>
+          <Box mt={2} mb={2}>
+            <TextField
+              id="loginPassword"
+              className={styles.authInput}
+              label="Password"
+              variant="outlined"
+            />
+          </Box>
+          <Button variant="contained" color="primary">
+            Log In
+          </Button>
+          <hr className={styles.authHR}></hr>
+
+          <Link to="/register" className={styles.bottomText}>
+            Already registered?
+          </Link>
+        </form>
+      </Container>
+    </>
+  );
 };
