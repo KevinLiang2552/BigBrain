9cf36e7036417f5d5b87263ed91a02d54234a5c4
nathan-lai234 <lai.nathan234@gmail.com>
2021-04-02 21:11:00 +1100

    add helper folder

--- frontend/src/components/auth/AuthInputs.jsx
+++ frontend/src/components/auth/AuthInputs.jsx
@@ -3,17 +3,19 @@ import PropTypes from 'prop-types';
 import styles from '../../styles/auth.module.css';
 import { InputAdornment, IconButton, TextField } from '@material-ui/core';
 import FeatherIcon from 'feather-icons-react';
+import { capitalizeFirstLetter } from '../../helpers/stringHelpers.js';
 
 export const DefaultInput = ({ type, handleFormChange }) => {
   DefaultInput.propTypes = {
     type: PropTypes.string,
     handleFormChange: PropTypes.func,
   };
+
   return (
     <TextField
       id="loginEmail"
       className={styles.authInput}
-      label={type}
+      label={capitalizeFirstLetter(type)}
       onChange={handleFormChange({ type })}
       variant="outlined"
     />
