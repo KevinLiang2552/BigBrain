78eb804c0779a81890514c25c68bc50edd585431
nathan-lai234 <lai.nathan234@gmail.com>
2021-04-02 18:29:05 +1100

    set browserServer code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
-
+import { LoginPage } from './routes/admin/Login.jsx';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 function App() {
-  return <div>hello</div>;
+  return (
+    <>
+      {/* This is the main browser router. 
+      Whenever you want to add a new page/route add it hear with a unique path */}
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <LoginPage></LoginPage>
+          </Route>
+          <Route path="/login">
+            <LoginPage></LoginPage>
+          </Route>
+          <Route path="/register">hey</Route>
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
