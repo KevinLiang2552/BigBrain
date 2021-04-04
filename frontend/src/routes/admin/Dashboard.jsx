24f2e2998f6d13d93d058bbb76c731bf3031af99
nathan-lai234 <lai.nathan234@gmail.com>
2021-04-04 15:57:32 +1000

    add dashboard header

--- frontend/src/routes/admin/Dashboard.jsx
+++ frontend/src/routes/admin/Dashboard.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
 import { getAuthToken } from '../../helpers/user.js';
+import {
+  Button,
+  Container,
+  Grid,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
+import styles from '../../styles/dashboard.module.css';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
 
 export const DashboardPage = () => {
   console.log('GET' + getAuthToken());
-  return <div>{getAuthToken()}</div>;
+  return (
+    <Container>
+      <Grid container className={styles.dashboardWrapper}>
+        <Grid xs={12}>
+          <Toolbar className={styles.dashboardHeader}>
+            <Button className={styles.createButton}>
+              <AddCircleIcon color="white" />
+              <Typography>Create Quiz</Typography>
+            </Button>
+            <div>{/* Extension add sort quiz here */}</div>
+          </Toolbar>
+        </Grid>
+        <Grid xs={12}>
+          <div></div>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
